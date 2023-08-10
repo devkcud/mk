3fb413c645b377fa86dcf4c2b3221d7d27aae702
André Luis (devkcud) <andrescalisejr@gmail.com>
2023-08-10 13:44:21 -0300

    minimal setup for later changes

--- main.go
+++ main.go
@@ -3,36 +3,27 @@ package main
 import (
 	"fmt"
 	"os"
-	"path"
-	"strings"
 )
 
+func createDir(path string) {
+	if os.MkdirAll(path, 0755) != nil {
+		fmt.Printf("couldn't create folder: %s\n", path)
+	}
+}
+
 func main() {
-	workingDir := ""
-
 	for _, name := range os.Args[1:] {
-		if strings.HasPrefix(name, ":") {
-			workingDir = name[1:]
-
-			if os.MkdirAll(workingDir, 0755) != nil {
-				fmt.Printf("couldn't create folder: %s\n", workingDir)
+		switch []rune(name)[0] {
+		case ':':
+			createDir(name[1:])
+			break
+		default:
+			if _, err := os.Stat(name); err == nil {
+				fmt.Printf("file already exists: %s\n", name[1:])
+			} else if os.WriteFile(name, []byte{}, 0644) != nil {
+				fmt.Printf("couldn't create folder: %s\n", name[1:])
 			}
-			continue
-		}
-
-		if strings.HasPrefix(name, "-") {
-			if os.MkdirAll(path.Join(workingDir, name[1:]), 0755) != nil {
-				fmt.Printf("couldn't create folder: %s\n", path.Join(workingDir, name[1:]))
-			}
-			continue
-		}
-
-		name = path.Join(workingDir, name)
-
-		if _, err := os.Stat(name); err == nil {
-			fmt.Printf("file already exists: %s\n", path.Join(workingDir, name[1:]))
-		} else if os.WriteFile(name, []byte{}, 0644) != nil {
-			fmt.Printf("couldn't create folder: %s\n", path.Join(workingDir, name[1:]))
+			break
 		}
 	}
 }
