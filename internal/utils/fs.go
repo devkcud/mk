00249a5e0b669c826ad808de65c3e53b0ef2eba3
André Luis (devkcud) <andrescalisejr@gmail.com>
2023-08-14 00:00:11 -0300

    chore: Separate utils into two files

---
+++ internal/utils/fs.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+func Mkdir(path string) {
+	if os.MkdirAll(path, 0755) != nil {
+		fmt.Printf("couldn't create folder: %s\n", path)
+	}
+}
+
+func Mkfile(path string) {
+	if _, err := os.Stat(path); err == nil {
+		fmt.Printf("path already exists: %s\n", path)
+	} else if os.WriteFile(path, []byte{}, 0644) != nil {
+		fmt.Printf("couldn't create file: %s\n", path)
+	}
+}
