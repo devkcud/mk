13d4afbf92ca964e63450fd006db0984461adfd4
André Luis (devkcud) <patommmmm@proton.me>
2023-08-16 13:28:12 -0300

    fix: Remove previous code of help

--- internal/help/help.go
+++ internal/help/help.go
@@ -2,18 +2,13 @@ package help
 
 import "fmt"
 
-const HelpString = `Usage: mk [+<folder>] [-] [%'<command>'] [filename]
+func ShowHelp() {
+	fmt.Println(`Usage: mk [+<folder>] [-] [%'<command>'] [filename]
 
 +<folder>    : Create a new directory and add it to the directory stack.
 -            : Remove the last added directory from the directory stack.
 %'<command>' : Runs a command in the last folder in the directory stack.
 filename     : Create an empty file with the specified name.
 
-Issues/PRs/Help: https://github.com/devkcud/mk`
-
-func ShowHelp(topic *string) {
-	// TODO: Add topic help (+; -; %; etc)
-	if topic == nil {
-		fmt.Println(HelpString)
-	}
+Issues/PRs/Help: https://github.com/devkcud/mk`)
 }
