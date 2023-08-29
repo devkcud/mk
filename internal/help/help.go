81accf9d41473cc162b5fe1f93f9f90116e7e591
André Luis (devkcud) <patommmmm@proton.me>
2023-08-29 15:34:07 -0300

    docs(help-menu): Update help menu :)

--- internal/help/help.go
+++ internal/help/help.go
@@ -3,9 +3,10 @@ package help
 import "fmt"
 
 func ShowHelp() {
-	fmt.Println(`Usage: mk [+<folder>] [-] [%'<command>'] [filename]
+	fmt.Println(`Usage: mk [+<folder>] [.<folder>] [-] [%'<command>'] [filename]
 
 +<folder>    : Create a new directory and add it to the directory stack.
+.<folder>    : Create a new directory without adding it to the directory stack.
 -            : Remove the last added directory from the directory stack.
 %'<command>' : Runs a command in the last folder in the directory stack.
 filename     : Create an empty file with the specified name.
