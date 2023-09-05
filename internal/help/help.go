7a4a7c7f92368775b16f083c6dd83eaaf302b11c
André Luis (devkcud) <patommmmm@proton.me>
2023-09-05 07:39:54 -0300

    update help function

--- internal/help/help.go
+++ internal/help/help.go
@@ -3,8 +3,6 @@ package help
 import "fmt"
 
 func ShowHelp() {
-    fmt.Printf("mk (%s) - by: devkcud\n", Version)
-
 	fmt.Println(`Usage: mk [.[folder]] [:<folder>] [[#]filename]
 
 ! OUTDATED ! Please refer to the source code until the help is updated
