80b385da2f20778ad560417a97daded0ad9d39cc
André Luis (devkcud) <patommmmm@proton.me>
2023-09-11 16:11:48 -0300

    docs: Update help and README

--- internal/help/help.go
+++ internal/help/help.go
@@ -3,9 +3,17 @@ package help
 import "fmt"
 
 func ShowHelp() {
-	fmt.Println(`Usage: mk [.[folder]] [:<folder>] [[#]filename]
+	fmt.Println(`Usage: mk [-flags] [..] [folder/] [files...]
 
-! OUTDATED ! Please refer to the source code until the help is updated
+Flags:
+    -help   Show this help menu
+    -quiet  Disable output
+
+Examples:
+    mk src/main.js
+    mk tests/
+    mk lib/ myclass.go otherclass.go andanotherclass.go
+    mk src/main.cpp . Makefile
 
 Issues/PRs/Help: https://github.com/devkcud/mk`)
 }
