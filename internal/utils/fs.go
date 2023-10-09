3d96d4cf678b52bab07cbfaa08f74de0acfdffa4
André devkcud Albanese <patommmmm@proton.me>
2023-10-09 10:21:45 -0300

    feat: Add fs logging

--- internal/utils/fs.go
+++ internal/utils/fs.go
@@ -1,20 +1,26 @@
 package utils
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/devkcud/mk/internal/mklog"
+	"github.com/fatih/color"
 )
 
 func CreateDir(path string) {
 	if os.MkdirAll(path, 0755) != nil {
-		fmt.Printf("couldn't create folder: %s\n", path)
+		mklog.Error("Couldn't create directory:", color.MagentaString(path))
 	}
+
+	mklog.Log("Created directory:", color.MagentaString(path))
 }
 
 func CreateFile(path string) {
 	if _, err := os.Stat(path); err == nil {
-		fmt.Printf("path already exists: %s\n", path)
+		mklog.Error("Path already exists:", color.MagentaString(path))
 	} else if os.WriteFile(path, []byte{}, 0644) != nil {
-		fmt.Printf("couldn't create file: %s\n", path)
+		mklog.Error("Couldn't create file:", color.MagentaString(path))
 	}
+
+	mklog.Log("Created file:", color.MagentaString(path))
 }
